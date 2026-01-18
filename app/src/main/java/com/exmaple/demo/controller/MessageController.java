5bbb9d62d6c6dadb9b60cabbf3cdf617953dfae0
Abhishekbhatt948 <Abhishekbhatt948@gmail.com>
2026-01-18 23:44:22 +0530

    Fix Spring package structure and imports for Kafka integration

--- app/src/main/java/com/exmaple/demo/controller/MessageController.java
+++ app/src/main/java/com/exmaple/demo/controller/MessageController.java
@@ -1,3 +1,10 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.KafkaProducerService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class MessageController {
 
