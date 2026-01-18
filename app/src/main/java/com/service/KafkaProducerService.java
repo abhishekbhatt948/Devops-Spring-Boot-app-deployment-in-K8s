468eb4a28d6206abec7a169b2baaa14aaa229a00
Abhishekbhatt948 <Abhishekbhatt948@gmail.com>
2026-01-18 23:21:49 +0530

    Integrate MySQL and Kafka with Spring Boot

---
+++ app/src/main/java/com/service/KafkaProducerService.java
@@ -0,0 +1,13 @@
+@Service
+public class KafkaProducerService {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public KafkaProducerService(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String message) {
+        kafkaTemplate.send("demo-topic", message);
+    }
+}
